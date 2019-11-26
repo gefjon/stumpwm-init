2943781824ba9284a5132c703bc3b7e89db6c71d
phoebe goldman <a@goldman-tribe.org>
2019-11-26 08:17:37 -0500

    modeline extensions
    use battery-portable and cpu in modeline; set date format

--- src/modeline.lisp
+++ src/modeline.lisp
@@ -5,12 +5,19 @@
 
 (load-module "hostname")
 (load-module "mem")
+(load-module "battery-portable")
+(load-module "cpu")
+
+(defparameter stumpwm:*time-modeline-string* "%a %e %b %k:%M:%S")
+(defparameter stumpwm:*time-format-string-default* "%a %e %b %Y %k:%M:%S")
 
 (defparameter *modeline-format-string*
-  "%h | %M | %d | %g | %v"
+  "%h | %M | %B | %C | %d | %g | %v"
   "left to right, these are:
-   %h hostname (supplied by hostname)
-   %M memory usage (supplied by mem)
+   %h hostname (supplied by `hostname')
+   %M memory usage (supplied by `mem')
+   %B battery (supplied by `battery-portable')
+   %c cpu usage (supplied by `cpu')
    %d date and time
    %g groups (virtual desktops)
    %v windows")
